3c8fa6dff35fe897eae68cbbdc3535f46d53964d
ASCA1ON <akash2468.ab@gmail.com>
2024-02-01 22:57:11 +0530

    added module data tll change

--- actions/home-page.js
+++ actions/home-page.js
@@ -35,3 +35,42 @@ export const getModuleData = (user) => {
   ];
   return response;
 };
+
+export const moduleData = {
+  vendorDetails: {
+    title: "Vendors Details",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    buttonText: "Inspect",
+  },
+  productCatalogue: {
+    title: "Product Catalogue",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    buttonText: "Inspect",
+  },
+  purchaseOrder: {
+    title: "Purchase Order",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    buttonText: "Inspect",
+  },
+  insights: {
+    title: "Insights",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    buttonText: "Inspect",
+  },
+  inventoryManage: {
+    title: "Inventory Manage",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    buttonText: "Inspect",
+  },
+  approvals: {
+    title: "Approvals",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    buttonText: "Inspect",
+  },
+};
